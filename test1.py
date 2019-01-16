ee9fef8d8021497445431140ae373a7f209b389e
William <williapb@gmail.com>
2019-01-16 16:41:00 +0000

    Implement HH model with K-channel only, and example for complete non-spatial HH model

--- test1.py
+++ test1.py
@@ -1,17 +1,40 @@
 from brian2 import *
 %matplotlib inline
 
-
 start_scope()
 
 # Params.
-tau=10ms  #
-
-Cm = (2*10**-8) farad  # Capacitance of membrane
-# dt=tau ?
-g_K = (60*10**-5) siemens
-g_l = (0.1 * e**-5) siemens  # electrical (linear) conductance of leak channel
+C_m = (2*10**-8)*farad  # Capacitance of membrane
+g_K = (60*10**-5)*siemens  # conductance of sodium channel
+g_l = (0.1 * e**-5)*siemens  # electrical (linear) conductance of leak channel
+V_l = -65*mV  # leak voltage
+V_K = -90*mV  # sodium leak voltage
+n = 0  # initially, no gates open?
 eqs='''
-I = Cm * (dVm/dt) + g_K * (Vm - VK) + g_l * (Vm - Vl)
-
+alpha_n = 0.01 * (10 - (V_m/mV)) / (exp((10 - (V_m/mV))/(10)) - 1) : 1
+beta_n = 0.125 * exp(-V_m/(80*mV)) : 1
+dn/dt = (alpha_n * (1 - n) - beta_n * n)/ms : 1
+dV_m/dt = (I_K - g_K * (V_m - V_K) - g_l * (V_m - V_l))/C_m : volt
+I_K : amp
 '''
+# I_K = g_K * n * (V_m ** 4) * (V_m - V_K) : amp
+# I = Cm * (dVm/dt) + g_K * (Vm - VK) + g_l * (Vm - Vl)
+
+group = NeuronGroup(1, eqs,
+                    threshold='V_m > -40*mV',
+                    refractory='V_m > -40*mV',
+                    method='exponential_euler')
+
+group.V_m = V_K
+statemon = StateMonitor(group, 'V_m', record=True)
+spikemon = SpikeMonitor(group, variables='V_m')
+figure(figsize=(9, 4))
+for l in range(5):
+    group.I_K = rand()*50*nA
+    run(10*ms)
+    axvline(l*10, ls='--', c='k')
+axhline(V_l/mV, ls='-', c='lightgray', lw=3)
+plot(statemon.t/ms, statemon.V_m[0]/mV, '-b')
+plot(spikemon.t/ms, spikemon.V_m/mV, 'ob')
+xlabel('Time (ms)')
+ylabel('V_m (mV)');
---
+++ test2.py
@@ -0,0 +1,39 @@
+start_scope()
+# Parameters
+area = 20000*umetre**2
+Cm = 1*ufarad*cm**-2 * area
+gl = 5e-5*siemens*cm**-2 * area
+El = -65*mV
+EK = -90*mV
+ENa = 50*mV
+g_na = 100*msiemens*cm**-2 * area
+g_kd = 30*msiemens*cm**-2 * area
+VT = -63*mV
+# The model
+eqs_HH = '''
+dv/dt = (gl*(El-v) - g_na*(m*m*m)*h*(v-ENa) - g_kd*(n*n*n*n)*(v-EK) + I)/Cm : volt
+dm/dt = 0.32*(mV**-1)*(13.*mV-v+VT)/
+    (exp((13.*mV-v+VT)/(4.*mV))-1.)/ms*(1-m)-0.28*(mV**-1)*(v-VT-40.*mV)/
+    (exp((v-VT-40.*mV)/(5.*mV))-1.)/ms*m : 1
+dn/dt = 0.032*(mV**-1)*(15.*mV-v+VT)/
+    (exp((15.*mV-v+VT)/(5.*mV))-1.)/ms*(1.-n)-.5*exp((10.*mV-v+VT)/(40.*mV))/ms*n : 1
+dh/dt = 0.128*exp((17.*mV-v+VT)/(18.*mV))/ms*(1.-h)-4./(1+exp((40.*mV-v+VT)/(5.*mV)))/ms*h : 1
+I : amp
+'''
+group = NeuronGroup(1, eqs_HH,
+                    threshold='v > -40*mV',
+                    refractory='v > -40*mV',
+                    method='exponential_euler')
+group.v = El
+statemon = StateMonitor(group, 'v', record=True)
+spikemon = SpikeMonitor(group, variables='v')
+figure(figsize=(9, 4))
+for l in range(10):
+    group.I = .1*nA # rand()*50*nA
+    run(10*ms)
+    axvline(l*10, ls='--', c='k')
+axhline(El/mV, ls='-', c='lightgray', lw=3)
+plot(statemon.t/ms, statemon.v[0]/mV, '-b')
+plot(spikemon.t/ms, spikemon.v/mV, 'ob')
+xlabel('Time (ms)')
+ylabel('v (mV)');
